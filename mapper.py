f1fc9ea868a77e2cc75b4e037795bd2fef89265c
Shubham Gupta <shubhamg14@gmail.com>
2019-04-23 20:31:50 -0400

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 import sys
+import csv
 
 # input comes from STDIN (standard input)
-for line in sys.stdin:
+reader=csv.reader(sys.stdin,delimiter=',')
+#Ignoring Header
+reader.next()
+for line in reader:
+	for word in line[24:29]:
+		if word.strip():
     # remove leading and trailing whitespace
-	line = line.strip()
-    # split the line into words
-	words = line.split(',')[-5:]
+			word = word.strip()
     # increase counters
-	for word in words:
-        # write the results to STDOUT (standard output);
+	 # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-		print '%s\t%s' % (word.lower(), 1)
+			print '%s\t%s' % (word.lower(), 1)
